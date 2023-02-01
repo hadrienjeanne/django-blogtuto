12653d55d0f59ca45106565bfa8a1c9dd4bbfbad
hadrienjeanne <hadrien.jeanne@free.fr>
2023-01-31 21:51:55 -0500

    Updated user profile page added a for to update name / email and picture surcharged the save method for the picture to reduce the picture size to 300x300

--- django_blogapp/users/forms.py
+++ django_blogapp/users/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from .models import Profile
 
 class UserRegisterForm(UserCreationForm):
     '''Custom form that extends the UserCreationForm to add mail'''
@@ -10,3 +11,19 @@ class UserRegisterForm(UserCreationForm):
         '''config'''
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+
+class UserUpdateForm(forms.ModelForm):
+    '''Custom form to update user profile username / email'''
+    email = forms.EmailField(required=True)
+
+    class Meta:
+        '''config'''
+        model = User
+        fields = ['username', 'email']
+
+class ProfileUpdateForm(forms.ModelForm):
+    '''Custom form to update user image'''
+    class Meta:
+        '''config'''
+        model = Profile
+        fields = ['image']
