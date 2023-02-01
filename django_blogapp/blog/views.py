ea717c26346de6bdd44eed8dc0bea9f59de1b8cd
hadrienjeanne <hadrien.jeanne@free.fr>
2023-01-31 22:12:35 -0500

    Modified the home page to use a class view of type ListView instead of a function to render the blog posts

--- django_blogapp/blog/views.py
+++ django_blogapp/blog/views.py
@@ -1,13 +1,18 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 from .models import Post
 
-# Create your views here.
 def home(request):
     '''home view'''
     context = {
         'posts': Post.objects.all()
     }
     return render(request, "blog/home.html", context)
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html' # by default: <app>/<model>_<viewtype>.html -> blog/post_list.html
+    context_object_name = 'posts'
 
 def about(request):
     '''about page view'''
