3ebd3e7b944e73ff25ef1e578f8976e3c2d6ce43
hadrienjeanne <hadrien.jeanne@free.fr>
2023-02-02 21:05:46 -0500

    Created a detail view for posts with django generic detail view specified route url for those detail views as post/postid added a link to the detail view in the home view

--- django_blogapp/blog/views.py
+++ django_blogapp/blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Post
 
 def home(request):
@@ -14,6 +14,11 @@ class PostListView(ListView):
     template_name = 'blog/home.html' # by default <app>/<model>_<viewtype>.html
     context_object_name = 'posts'
     ordering = ['-date_posted']
+
+class PostDetailView(DetailView):
+    # template by default blog/post_detail.html
+    model = Post
+
 
 def about(request):
     '''about page view'''
