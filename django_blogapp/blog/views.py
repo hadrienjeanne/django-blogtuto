319d9dd7d50c632a18f00972d8e7ebd50754c6f1
hadrienjeanne <hadrien.jeanne@free.fr>
2023-02-02 21:41:24 -0500

    Added a delete page for posts

--- django_blogapp/blog/views.py
+++ django_blogapp/blog/views.py
@@ -2,7 +2,13 @@ from typing import Optional
 from django.forms import BaseModelForm
 from django.http import HttpResponse
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView, CreateView, UpdateView
+from django.views.generic import (
+    ListView,
+    DetailView,
+    CreateView,
+    UpdateView,
+    DeleteView
+    )
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from .models import Post
 
@@ -40,6 +46,16 @@ class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     def form_valid(self, form: BaseModelForm) -> HttpResponse:
         form.instance.author = self.request.user
         return super().form_valid(form)
+
+    def test_func(self) -> Optional[bool]:
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    success_url = "/"
 
     def test_func(self) -> Optional[bool]:
         post = self.get_object()
