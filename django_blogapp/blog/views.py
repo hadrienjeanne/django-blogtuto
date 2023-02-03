7f16774202afbcd91e02b7df634fc4f74b45792e
hadrienjeanne <hadrien.jeanne@free.fr>
2023-02-02 21:31:57 -0500

    Created an update view made sure that only the author could update post

--- django_blogapp/blog/views.py
+++ django_blogapp/blog/views.py
@@ -1,7 +1,9 @@
+from typing import Optional
 from django.forms import BaseModelForm
 from django.http import HttpResponse
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from .models import Post
 
 def home(request):
@@ -21,7 +23,7 @@ class PostDetailView(DetailView):
     # template by default blog/post_detail.html
     model = Post
 
-class PostCreateView(CreateView):
+class PostCreateView(LoginRequiredMixin, CreateView):
     # template by default blog/post_form.html
     model = Post
     fields = ['title', 'content']
@@ -29,8 +31,21 @@ class PostCreateView(CreateView):
     def form_valid(self, form: BaseModelForm) -> HttpResponse:
         form.instance.author = self.request.user
         return super().form_valid(form)
- 
 
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    # template by default blog/post_form.html
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form: BaseModelForm) -> HttpResponse:
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self) -> Optional[bool]:
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
 
 def about(request):
     '''about page view'''
