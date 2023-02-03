c4020e07112f620bc2e58b10401b836c4d640be8
hadrienjeanne <hadrien.jeanne@free.fr>
2023-02-02 21:18:35 -0500

    added a page to create new posts saves the post to the db and redirects to the post detail on submit

--- django_blogapp/blog/views.py
+++ django_blogapp/blog/views.py
@@ -1,5 +1,7 @@
+from django.forms import BaseModelForm
+from django.http import HttpResponse
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView
 from .models import Post
 
 def home(request):
@@ -18,6 +20,16 @@ class PostListView(ListView):
 class PostDetailView(DetailView):
     # template by default blog/post_detail.html
     model = Post
+
+class PostCreateView(CreateView):
+    # template by default blog/post_form.html
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form: BaseModelForm) -> HttpResponse:
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+ 
 
 
 def about(request):
